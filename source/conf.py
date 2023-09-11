12a244776031d55ff295a9d5c20fbf25a0be0f50
Simone Orru <simone.orru@secomind.com>
2023-09-11 10:15:03 +0200

    Documentation using sphinx and markdown
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

---
+++ source/conf.py
@@ -0,0 +1,35 @@
+# Copyright 2024 SECO Mind Srl
+#
+# SPDX-License-Identifier: CC0-1.0
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Astarte SDKs'
+copyright = '2024 SECO Mind Srl'
+author = 'Simone Orru'
+release = ''
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_copybutton', 'myst_parser', 'sphinx_design', 'sphinx_rtd_theme']
+
+myst_enable_extensions = ["colon_fence"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_favicon = 'images/mascotte.svg'
+
+html_static_path = ['_static']
